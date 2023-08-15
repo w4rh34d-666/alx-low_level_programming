6faf1365701d3e6a8afd4d283e9b67372d0524c5
w4rh34d-666 <lil.mizoo@gmail.com>
2023-08-16 01:24:01 +0300

    resolving task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphbet
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char g = 'a';
+
+	while (g <= 'z')
+	{
+		_putchar(g);
+		g++;
+
+	}
+	_putchar('\n');
+
+}
